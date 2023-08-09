ff038cdc0e0190178d27969f22bf456ad4a24ed6
Yağmur Yalçın <yagmuryalcin26@hotmail.com>
2023-08-09 13:13:30 +0300

    day2

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using dotnet.Services.CharacterService;
+using dotnet.Dtos;
+
+namespace dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+       
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+          [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+        
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
+        {
+            
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+    }
+}
---
+++ Mode/Character.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet.Mode
+{
+    public class Character
+    {
+       
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strength { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        // public Rpg Class { get; set; } = Rpg.Knight;
+    }
+}
+        
+    
